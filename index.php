90a607d0884ec3d3b709d18a0b696d17b8d4dde5
ittekikun <ittekikun@hotmail.co.jp>
2015-02-08 22:15:27 +0900

    とりまランキング表示機構完成

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
-<ul>
-<?php
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+
+<table class="table">
+      <caption>ニコニコ動画ランキング</caption>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>サムネイル</th>
+          <th>動画名</th>
+          <th>アップロード日時</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
 $rss=simplexml_load_file('http://www.nicovideo.jp/ranking/fav/daily/all?rss=2.0&lang=ja-jp');
 $i=0;
-foreach($rss->channel->item as $item){
+foreach($rss->channel->item as $item)
+{
 	if($i++==10){
 		break;
 	}
@@ -14,8 +37,12 @@ foreach($rss->channel->item as $item){
 	$img_url=substr($description, $first,$last-$first);//指定の場所から指定の文字数分を引いた文字を抽出、画像のURL取得
 
 	$date=date('Y/m/d H:i', strtotime($item->pubDate));
-	print '<li><img src="'.$img_url.'" width="70" align="left" /><a href="'.$link.'" >'.$title.'（'.$date.'）</a></li>';
+
+	print '<tr><th scope="row">'.$i.'位</th><td><img src="'.$img_url.'" width="70" /></td><td><a href="'.$link.'" >'.$title.'</a></td><td>'.$date.'</td>';
+
 	print(PHP_EOL);
-}
-?>
-</ul>
+        }
+        ?>
+      </tbody>
+    </table>
+</html>
