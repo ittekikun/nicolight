0248f0da9131610eacbcefc20e0ac24deee4bf9e
nue5493 <koishu5493@gmail.com>
2015-02-08 22:26:13 +0900

    インデント整理＋タグの追加

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
-<html>
-<head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-</head>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<title>nicolight(仮)</title>
+		<meta charset="utf-8">
+	</head>
 
-<table class="table">
-      <caption>ニコニコ動画ランキング</caption>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>サムネイル</th>
-          <th>動画名</th>
-          <th>なんかよくわからない日時</th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php
-$rss=simplexml_load_file('http://www.nicovideo.jp/ranking/fav/daily/all?rss=2.0&lang=ja-jp');
-$i=0;
-foreach($rss->channel->item as $item)
-{
-	if($i++==100){
-		break;
-	}
-	$link=$item->link;
-	$title=$item->title;
-	$description=$item->description;
-	$first=strpos($description, 'src="')+5;//srcの5文字分
-	$last=strpos($description, 'width="94"')-2;//空白と"の分
-	$img_url=substr($description, $first,$last-$first);//指定の場所から指定の文字数分を引いた文字を抽出、画像のURL取得
+	<body>
+		<table class="table">
+			<caption>ニコニコ動画ランキング</caption>
+				<thead>
+			        <tr>
+			          <th>#</th>
+			          <th>サムネイル</th>
+			          <th>動画名</th>
+			          <th>なんかよくわからない日時</th>
+			        </tr>
+				</thead>
+		      <tbody>
+		      <?php
+				$rss=simplexml_load_file('http://www.nicovideo.jp/ranking/fav/daily/all?rss=2.0&lang=ja-jp');
+				$i=0;
+				foreach($rss->channel->item as $item)
+				{
+					if($i++==100){
+						break;
+					}
+					$link=$item->link;
+					$title=$item->title;
+					$description=$item->description;
+					$first=strpos($description, 'src="')+5;//srcの5文字分
+					$last=strpos($description, 'width="94"')-2;//空白と"の分
+					$img_url=substr($description, $first,$last-$first);//指定の場所から指定の文字数分を引いた文字を抽出、画像のURL取得
 
-	$date=date('Y/m/d H:i', strtotime($item->pubDate));
+					$date=date('Y/m/d H:i', strtotime($item->pubDate));
 
-	print '<tr><th scope="row">'.$i.'位</th><td><img src="'.$img_url.'" width="70" /></td><td><a href="'.$link.'" >'.$title.'</a></td><td>'.$date.'</td>';
+					print '<tr><th scope="row">'.$i.'位</th><td><img src="'.$img_url.'" width="70" /></td><td><a href="'.$link.'" >'.$title.'</a></td><td>'.$date.'</td>';
 
-	print(PHP_EOL);
-        }
-        ?>
-      </tbody>
-    </table>
+					print(PHP_EOL);
+		        }
+		      ?>
+		      </tbody>
+		</table>
+	</body>
 </html>
